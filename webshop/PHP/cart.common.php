c928fcaf813ba22437582981be1bda2e0db27a31
Vasla <eripet05@hotmail.com>
2025-06-06 01:46:03 +0200

    fix

--- webshop/PHP/cart.common.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("C:/xampp/htdocs/webshop/xajax/xajax_core/xajax.inc.php");
-
-$xajax = new xajax("cart.server.php");
-$xajax->configure('javascript URI', '../xajax/');
-$xajax->configure('debug', FALSE);
-
-$xajax->register(XAJAX_FUNCTION, "addtocart");
-$xajax->register(XAJAX_FUNCTION, "changePosition");
